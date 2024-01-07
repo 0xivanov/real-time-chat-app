530e7aef267a4fe354cfde40aa47bc718c7c5601
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-01-07 11:10:34 +0200

    Revert "initial impl"
    This reverts commit b19113d8d82ca7f9ba8ad9b85860b962e4cbe286.

--- server/model/message.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Message struct {
-	Content  string `json:"content"`
-	RoomID   string `json:"room_id"`
-	Username string `json:"username"`
-}
--- server/model/room.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Room struct {
-	ID      string             `json:"id" gorm:"primaryKey"`
-	Name    string             `json:"name" gorm:"unique"`
-	Clients map[string]*Client `json:"clients,omitempty" gorm:"-"`
-}
