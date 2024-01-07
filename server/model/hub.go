1b142d23e7ee241f63cd1aa1e41a90c22c897f9c
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-01-07 10:51:39 +0200

    cleanup

--- server/model/hub.go
+++ server/model/hub.go
@@ -14,7 +14,7 @@ func NewHub(rooms map[string]*Room) *Hub {
 		Rooms:      rooms,
 		Register:   make(chan *Client),
 		Unregister: make(chan *Client),
-		Broadcast:  make(chan *Message),
+		Broadcast:  make(chan *Message, 5),
 	}
 }
 
@@ -30,27 +30,25 @@ func (h *Hub) Run() {
 			}
 		case client := <-h.Unregister:
 			if _, ok := h.Rooms[client.RoomID]; ok {
-				println("USER LEFT2")
 				room := h.Rooms[client.RoomID]
 				if _, ok := room.Clients[client.ID]; ok {
-					println("USER LEFT1")
 					if len(h.Rooms[client.RoomID].Clients) != 0 {
-						println("USER LEFT3")
+						println(&room.Clients)
+						println(client.ID)
 						// broadcast that the client has left the room
-						// h.Broadcast <- &Message{
-						// 	Content:  "user has left the chat",
-						// 	Username: client.Username,
-						// 	RoomID:   client.RoomID,
-						// }
+						h.Broadcast <- &Message{
+							Content:  "user has left the chat",
+							Username: client.Username,
+							RoomID:   client.RoomID,
+						}
 					}
-					println(room.Clients)
-					println(client.ID)
 					delete(room.Clients, client.ID)
 					close(client.Message)
 					println("USER LEFT")
 				}
 			}
 		case message := <-h.Broadcast:
+			fmt.Println("DISSSS")
 			fmt.Println(message)
 			fmt.Println(message.Content)
 			if _, ok := h.Rooms[message.RoomID]; ok {
